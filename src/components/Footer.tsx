f00481d5d13ab7521dca56fa2a6f9de8db666217
Ashutosh Soni <ashutoshsoni0699@gmail.com>
2024-01-30 21:40:18 +0530

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <section className={`${styles.flexCenter}`}>
       <div className="w-full justify-between items-center pt-6 pb-1 bg-black">
         <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-          2021 HooBank. All Rights Reserved.
+          2024 @Sahil. All Rights Reserved.
         </p>
       </div>
     </section>
